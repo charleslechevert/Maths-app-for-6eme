6f5c4c1f3ca743dfba192450de80e949a75f55a6
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-09-06 22:49:39 +0200

    V2 : création d'un deuxième jeu sur les zéros inutiles (non fini) + toujours pas de design

---
+++ ch1_g2/ch1_g2.js
@@ -0,0 +1,32 @@
+var values = [0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9];
+var randomValue=0;
+
+
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit1").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit2").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit3").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit4").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit5").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit6").innerHTML = values[randomValue];
+
+randomValue = Math.floor(Math.random()*18);
+document.getElementById("random_digit7").innerHTML = values[randomValue];
+
+
+
+/*Reset new number */
+function reset() {
+
+}
