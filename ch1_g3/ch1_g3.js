53b7ce702793a4099908f0aaa06720755650c983
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-09-09 11:44:45 +0200

    Work on game3

--- ch1_g3/ch1_g3.js
+++ ch1_g3/ch1_g3.js
@@ -2,6 +2,8 @@ var number = [0,0.2,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2,2.2,2.4,2.6,2.8,3]
 var playerDigit = 4
 
 
+// Generate random numbers associated to parrots
+
 var randomNumber = Math.floor(Math.random()*16);
 document.getElementById('parrot1').innerHTML = number[randomNumber];
 
@@ -14,12 +16,22 @@ document.getElementById('parrot3').innerHTML = number[randomNumber];
 var randomNumber = Math.floor(Math.random()*16);
 document.getElementById('parrot4').innerHTML = number[randomNumber];
 
-const mybtn = document.getElementById('parrotContainer1');
-mybtn.addEventListener('click', clickNumber);
 
-function clickNumber() {
-   var parrotNumber = document.getElementById("parrot1").innerHTML;
-   console.log(parrotNumber)
-}
+//Manage drag and drop function
+
+function allowDrop(ev) {
+   ev.preventDefault();
+ }
+ 
+ function drag(ev) {
+   ev.dataTransfer.setData("text", ev.target.id);
+ }
+ 
+ function drop(ev) {
+   ev.preventDefault();
+   var data = ev.dataTransfer.getData("text");
+   console.log(data)
+   ev.target.appendChild(document.getElementById(data));
+ }
 
 
