34f0878b03b44735425414ad911efafc2b9a5b69
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-09-15 21:36:58 +0200

    g5 almost done

--- ch3_g2/ch3_g2.js
+++ ch3_g2/ch3_g2.js
@@ -2,6 +2,7 @@ app = {
     countscore : 0,
     init() {
         app.numberGenerated()
+  
         
 
       },
@@ -11,112 +12,160 @@ app = {
             for(let iterateNumber=0;iterateNumber<40;iterateNumber++) {
                 const newDiv =  document.createElement('div');
                 newDiv.classList.add('number');
+                newDiv.classList.add('row' + iterateRows);
+
                 newDiv.textContent = Math.ceil(Math.random()*50)
 
                 document.getElementById(`container${iterateRows + 1}`).append(newDiv);
 
                 document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
-
-                app.moveSprite()
             
             }
         }
+        app.moveSprite1()
+
 
     },
-    moveSprite() { //this function will make the sprite move if the right log is clicked
+    
 
-        authorization = [true,false,false,false,false] 
-
-        
-
-        var logElm = document.querySelectorAll('.number') //put in a table all the numbers
-        for(let i=0;i<logElm.length;i++) { //loop to add a listener for all the numbers
-            logElm[i].addEventListener('click', function(event) {
-
-                if(authorization[0]) {
-        
-
+    moveSprite1() { //this function will make the sprite move if the right log is clicked
+        console.log('okd')
+        if(document.querySelector(".sprite-img").parentElement.classList == "sprite") {
+            for(let i=0;i<40;i++) { //loop to add a listener and execute fct for one row
+                document.querySelectorAll('.row0')[i].addEventListener('click', (event) => {
+                
                     var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
                     if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
+                        app.countscore++; //Count the score*
+                        document.querySelector('#score').textContent = app.countscore;
                         var spriteElm =  document.querySelector('.sprite-img')
                         document.querySelector('#container1').replaceChild(spriteElm, event.target)
-                        app.countscore++; //Count the score
-                        document.querySelector('#score').textContent = app.countscore;
-                        authorization = [false,true,false,false,false]  //change authorization
                         document.getElementById("container1").style.animationPlayState="paused";
                         document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
-                        
-                        
-                        
-
+                        app.moveSprite2()
 
                     } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
                         app.countscore--;
                         document.querySelector('#score').textContent = app.countscore;
-                        }
+                    } 
+                    
+                })
+                 
+            }  
+               
 
-                    }
+        }
+    },
+
+        moveSprite2() { //this function will make the sprite move if the right log is clicked
+            console.log('okii')
+    
+    
+    
+            if(document.querySelector(".sprite-img").parentElement.id == "container1") {
+                console.log('okiidoki')
+                for(let i=0;i<40;i++) { //loop to add a listener and execute fct for one row
+                    document.querySelectorAll('.row1')[i].addEventListener('click', (event) => {
+                    
+                        var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
+                        if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
+                            app.countscore++; //Count the score*
+                            document.querySelector('#score').textContent = app.countscore;
+                            var spriteElm =  document.querySelector('.sprite-img')
+                            document.querySelector('#container2').replaceChild(spriteElm, event.target)
+                            document.getElementById("container2").style.animationPlayState="paused";
+                            document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
+                            app.moveSprite3()
+    
+                            
+        
+                        } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
+                            app.countscore--;
+                            document.querySelector('#score').textContent = app.countscore;
+                        } 
+                        
+                    })
+                     
+                }  
+                   
+    
+            }
+        },
 
 
-                else if(authorization[1]) { 
+        moveSprite3() { //this function will make the sprite move if the right log is clicked
 
-                    var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
-                    if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
-                        var spriteElm =  document.querySelector('.sprite-img')
-                        document.querySelector('#container2').replaceChild(spriteElm, event.target)
-                        app.countscore++; //Count the score
-                        document.querySelector('#score').textContent = app.countscore; //display score
-                        authorization = [false,false,true,false,false] 
-                        document.getElementById("container2").style.animationPlayState="paused";
-                        document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
+            if(document.querySelector(".sprite-img").parentElement.id == "container2") {
+                for(let i=0;i<40;i++) { //loop to add a listener and execute fct for one row
+                    document.querySelectorAll('.row2')[i].addEventListener('click', (event) => {
+                    
+                        var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
+                        if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
+                            app.countscore++; //Count the score*
+                            document.querySelector('#score').textContent = app.countscore;
+                            var spriteElm =  document.querySelector('.sprite-img')
+                            document.querySelector('#container3').replaceChild(spriteElm, event.target)
+                            document.getElementById("container3").style.animationPlayState="paused";
+                            document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
+                            app.moveSprite4()
+    
+                            
+        
+                        } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
+                            app.countscore--;
+                            document.querySelector('#score').textContent = app.countscore;
+                        } 
+                        
+                    })
+                     
+                }  
+                   
+    
+            }
+        },
 
-                    } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
-                        app.countscore--;
-                        document.querySelector('#score').textContent = app.countscore;
-                        }
-                }
+        moveSprite4() { //this function will make the sprite move if the right log is clicked
+            console.log('okd')
+    
+    
+    
+            if(document.querySelector(".sprite-img").parentElement.id == "container3") {
+                for(let i=0;i<40;i++) { //loop to add a listener and execute fct for one row
+                    document.querySelectorAll('.row3')[i].addEventListener('click', (event) => {
+                    
+                        var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
+                        if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
+                            app.countscore++; //Count the score*
+                            document.querySelector('#score').textContent = app.countscore;
+                            var spriteElm =  document.querySelector('.sprite-img')
+                            document.querySelector('#container4').replaceChild(spriteElm, event.target)
+                            document.getElementById("container4").style.animationPlayState="paused";
+                            document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
+                            app.moveSprite5()
+    
+                            
+        
+                        } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
+                            app.countscore--;
+                            document.querySelector('#score').textContent = app.countscore;
+                        } 
+                        
+                    })
+                     
+                }  
+                   
+    
+            }
+        },
 
 
-                else if(authorization[2]) {
+   
 
-                    var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
-                    if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
-                        var spriteElm =  document.querySelector('.sprite-img')
-                        document.querySelector('#container3').replaceChild(spriteElm, event.target)
-                        app.countscore++; //Count the score
-                        document.querySelector('#score').textContent = app.countscore;
-                        authorization = [false,false,false,true,false] 
-                        document.getElementById("container3").style.animationPlayState="paused";
-                        document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
-
-                    }  else if (app.countscore > 0 && event.target.textContent % divisorElm != 0) {
-                        app.countscore--; //Count the score
-                        document.querySelector('#score').textContent = app.countscore; //display score
-
-                    }
-                }
-
-                else if(authorization[3]) {
-
-                    var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
-                    if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
-                        var spriteElm =  document.querySelector('.sprite-img')
-                        document.querySelector('#container4').replaceChild(spriteElm, event.target)
-                        app.countscore++; //Count the score
-                        document.querySelector('#score').textContent = app.countscore;
-                        authorization = [false,false,false,false,true] 
-                        document.getElementById("container4").style.animationPlayState="paused";
-                        document.querySelector('.multiple__number').textContent = Math.ceil(Math.random()*6)
-
-                    } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
-                        app.countscore--;
-                        document.querySelector('#score').textContent = app.countscore;
-                        }
-                }
-
-                else if(authorization[4]) {
+    moveSprite5() {
+        if(document.querySelector(".sprite-img").parentElement.id == "container4") {
+            for(let i=0;i<40;i++) { //loop to add a listener and execute fct for one row
+                document.querySelectorAll('.row4')[i].addEventListener('click', function(event) {
                 
-
                     var divisorElm = document.querySelector('.multiple__number').textContent //Get the divisor valuer 
                     if (event.target.textContent % divisorElm == 0) { //Check if the clicked element is multiple of the bottom-right number
                         var spriteElm =  document.querySelector('.sprite-img')
@@ -125,9 +174,7 @@ app = {
                         
                         app.countscore++; //Count the score
                         document.querySelector('#score').textContent = app.countscore;
-                        authorization = [true,false,false,false,false] 
                         document.getElementById("container5").style.animationPlayState="paused";
-                        console.log('yo')
                         
 
                         const boxes = document.querySelectorAll('.number');
@@ -142,24 +189,25 @@ app = {
                         document.getElementById("container5").style.animationPlayState="running";
 
                         document.querySelector(".sprite").append(document.querySelector('.sprite-img'))
-
-                       
-                    
                         app.numberGenerated()
 
                     } else if (event.target.textContent % divisorElm != 0 && app.countscore > 0) {
                         app.countscore--;
                         document.querySelector('#score').textContent = app.countscore;
                         }
-                }
+                })
 
 
-            })
+            }
         }
         
         
+    
 
     }
 }
 
 app.init()
+
+
+
