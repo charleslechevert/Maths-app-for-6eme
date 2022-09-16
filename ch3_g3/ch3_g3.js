045469eda129bf926097d3c846d831e6a9965cc5
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-09-16 18:34:02 +0200

    yo

--- ch3_g3/ch3_g3.js
+++ ch3_g3/ch3_g3.js
@@ -1,13 +1,45 @@
 app = {
     gridElm: document.querySelector('.grid'),
     divisor : undefined,
+    upPosition: '',
+    leftPosition: '',
+    rightPosition: '',
+    countdown: 90,
+    countScore: 0,
 
 
     init() {
         app.createGrid()
+        app.startGame()
         app.placeSprite()
-        app.generateNearNumber()
+        
 
+    },
+    startGame() {
+        document.getElementById('start').addEventListener('click', (event) => {
+            console.log('yo')
+            state = false;
+            if (state==false) { //state manage the fact you can only once press start
+                state = true;
+                document.getElementById('start').style.color = 'transparent';
+                setInterval(app.timer,1000);
+                app.generateNearNumber()
+        }
+        })
+    },
+    
+    /*Manage time */
+    
+    timer() {
+        document.getElementById('countdown').innerHTML = app.countdown;
+    
+        if (app.countdown==0) {
+            clearInterval(timer);
+            alert("Gamer Over. Score: " + countScore);
+            location.reload();
+    
+        }
+        app.countdown--;
     },
 
     createGrid(gridSize = 8, cellSize = 25) {
@@ -58,26 +90,27 @@ app = {
         
       },
       generateNearNumber(){
+        app.generateDivisor ()
         var spritePosition = document.querySelector('.sprite').parentElement.id
         console.log('yo')
         
         //generate the position of the neighbour 
-        var upPosition = parseInt(spritePosition) - 8
-        var leftPosition = parseInt(spritePosition) - 1 
-        var rightPosition = parseInt(spritePosition) + 1 
+        app.upPosition = parseInt(spritePosition) - 8
+        app.leftPosition = parseInt(spritePosition) - 1 
+        app.rightPosition = parseInt(spritePosition) + 1 
 
         // generate the neighbour number : 1 multiple 2 not-multiple
         var number1 = 0
-        while (number1 % 2 == 0) {
+        while (number1 % app.divisor == 0) {
             number1 = Math.ceil(Math.random()*50)
         }
         var number2 = 0
-        while (number2 % 2 == 0) {
+        while (number2 % app.divisor == 0) {
             number2 = Math.ceil(Math.random()*50)
         }
 
         var number3 = undefined
-        while (number3 % 2 != 0) {
+        while (number3 % app.divisor != 0) {
             number3 = Math.ceil(Math.random()*50)
         }
         
@@ -88,11 +121,9 @@ app = {
         console.log(numbers)
 
         //Connect the random numbers to the neigbhour cells
-        document.getElementById(upPosition).textContent = numbers[0]
-        console.log(document.getElementById(leftPosition))
-        document.getElementById(leftPosition).textContent = numbers[1]
-        console.log(document.getElementById(rightPosition))
-        document.getElementById(rightPosition).textContent = numbers[2]
+        document.getElementById(app.upPosition).textContent = numbers[0]
+        document.getElementById(app.leftPosition).textContent = numbers[1]
+        document.getElementById(app.rightPosition).textContent = numbers[2]
         
         
       },
@@ -109,19 +140,41 @@ app = {
     },
 
     moveSprite(event) {
-        if (event.target.textContent != '' && event.target.textContent % 2 == 0) {
+        if (event.target.textContent != '' && event.target.textContent % app.divisor == 0) {
             event.target.textContent = ''
-            event.target.append(document.querySelector('.sprite'))
-            app.generateNearNumber()
+            event.target.style.backgroundImage = 'none'
+
+            app.countScore++; //up the score
+            document.getElementById('score').textContent = app.countScore //display new score
             
 
-        } 
+            document.getElementById(app.upPosition).textContent = ""
+            document.getElementById(app.rightPosition).textContent = ""
+            document.getElementById(app.leftPosition).textContent = ""
+            event.target.append(document.querySelector('.sprite'))
+            app.generateNearNumber()
+        }
+
+        else if (event.target.textContent != '') {
+            app.countScore--; //down the score
+            document.getElementById('score').textContent = app.countScore//display new score
+
+            event.target.style.backgroundImage = "url(vortex.png)" //create vortex effect if wrong
+            event.target.textContent = ''
+
+            setTimeout(() => event.target.style.backgroundImage = "url(pavement.png)", 800) //create vortex effect if wrong
+            
+        
+
+
+        }
+
 
         
         
     },
     generateDivisor () {
-        app.divisor = Math.ceil(Math.random()*11)
+        app.divisor = Math.ceil(Math.random()*10 + 1)
         document.querySelector('.divisor__number').textContent = app.divisor
         console.log(app.divisor)
     }
