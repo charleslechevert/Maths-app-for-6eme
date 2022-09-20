0bb79dcfabd678a2173c04ecaa18affdf15f331c
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-09-20 16:34:20 +0200

    fix bugs

--- ch3_g1/ch3_g1.js
+++ ch3_g1/ch3_g1.js
@@ -1,202 +1,185 @@
-var randomSize = null; 
-var numberI = 0;
-var countScore = 0;
-
-//GENERATE GRID____________________________________________
-
-function generateGrid() {
-
-    var numFruit = Math.ceil(Math.random()*8) //Select randomly fruit to displat
-
-
-    var randomSize = Math.floor(Math.random() * (8 - 4) + 4)
-    console.log(randomSize)
-    var sizeCell = (100 / randomSize)
-    console.log(sizeCell)
-    for(let i=0;i<(randomSize**2);i++) {
-
-        //this part generate the grid with right size for the cell in order to take all the room
-        var newCell = document.createElement('div');
-
-        newCell.classList.add('cell')
-        newCell.style.width = sizeCell + 'vw'
-        newCell.style.height = sizeCell + 'vw'
-        document.querySelector('.grid').appendChild(newCell)
-
-        //This part generate random object on the grid
-        var random_boolean = Math.random() < 0.5;
-        if (random_boolean==true) {
-            var elem = document.createElement("div");
-            elem.setAttribute("draggable", 'true');
-            document.querySelectorAll(".cell")[i].appendChild(elem);
-            elem.setAttribute("class", 'fill'); 
-            elem.style.height= sizeCell + 'vw'
-            elem.style.width= sizeCell + 'vw'
-            elem.style.backgroundImage = 'url(fruit' + numFruit  + '.png)'
-            
-            
-
-
-
-
-            
-            
-        } 
+app = {
+    randomSize : null,
+    countScore : 0,
+    countdown:90,
+    startGame() { //It starts the game
+        document.getElementById('start').addEventListener('click', ()=> {
+            state = false;
+            if (state==false) { //state manage the fact you can only once press start
+                state = true;
+                document.getElementById('start').style.color = 'transparent';
+                setInterval(app.timer,1000);
+                app.generateGrid()
+                app.generatePad()
+                app.giveAnswer()
+                app.drag()
         
-
-    }
+            }
+        })
     
-}
-
-//GENERATE PAD_________________________________________________________________________________________________
-function generatePad() {
-    for(let i=0;i<10;i++) {
-        var newDiv = document.createElement('div');
-        newDiv.classList.add('numberButton');
-        newDiv.textContent=i;
-        document.querySelector('.pad').append(newDiv)
-    }
+    },
+    generateGrid() { //GENERATE GRID
+        var numFruit = Math.ceil(Math.random()*8) //Select randomly fruit to displat
 
 
-}
+        var randomSize = Math.floor(Math.random() * (8 - 4) + 4)
+        if(window.screen.width < 500) { //Manage responsivness (if phone we are in VW whereas in laptop we are in px)
+            var sizeCell = (100 / randomSize)
+            var sizeCellVwPx = sizeCell + 'vw'
+          
+        } else if(window.screen.width >= 500) {
+            console.log('ya')
+            var sizeCell = (500 / randomSize)
+            var sizeCellVwPx = sizeCell + 'px'
+        }
 
+        for(let i=0;i<(randomSize**2);i++) {
+    
+            //this part generate the grid with right size for the cell in order to take all the room
+            var newCell = document.createElement('div');
+    
+            newCell.classList.add('cell')
+            newCell.style.width = sizeCellVwPx
+            newCell.style.height = sizeCellVwPx
+            document.querySelector('.grid').appendChild(newCell)
+    
+            //This part generate random object on the grid
+            var random_boolean = Math.random() < 0.5;
+            if (random_boolean==true) {
+                var elem = document.createElement("div");
+                elem.setAttribute("draggable", 'true');
+                document.querySelectorAll(".cell")[i].appendChild(elem);
+                elem.setAttribute("class", 'fill'); 
+                elem.style.height= sizeCellVwPx
+                elem.style.width= sizeCellVwPx
+                elem.style.backgroundImage = 'url(fruit' + numFruit  + '.png)'
+            }    
+        }
 
-
-// DRAG AND DROP________________________________________________________________________________________________
-
-function drag() {
-    const fill = document.querySelectorAll('.fill');
-    const empties = document.querySelectorAll('.cell');
-
-    // Fill listeners
-
-    for(let i=0;i<fill.length;i++) {
-
-
-
-    fill[i].addEventListener('dragstart', dragStart);
-    fill[i].addEventListener('dragend', dragEnd);
-
-
-    // Loop through empty boxes and add listeners
-    for (const empty of empties) {
-        empty.addEventListener('dragover', dragOver);
-        empty.addEventListener('dragenter', dragEnter);
-        empty.addEventListener('dragleave', dragLeave);
-        empty.addEventListener('drop', dragDrop);
-    }
-
-    function dragStart() {
-        this.className += ' hold';
-        setTimeout(() => (this.className = 'invisible'), 0);
-        numberI=i;
-
+    },
+    generatePad() { //Generate the pad
+        for(let i=0;i<10;i++) {
+            var newDiv = document.createElement('div');
+            newDiv.classList.add('numberButton');
+            newDiv.textContent=i;
+            document.querySelector('.pad').append(newDiv)
+        }
+    },
+    drag() {
+        const fill = document.querySelectorAll('.fill');
+        const empties = document.querySelectorAll('.cell');
+    
+        // Fill listeners
+    
+        for(let i=0;i<fill.length;i++) {
+    
+    
+    
+        fill[i].addEventListener('dragstart', dragStart);
+        fill[i].addEventListener('dragend', dragEnd);
+    
+    
+        // Loop through empty boxes and add listeners
+        for (const empty of empties) {
+            empty.addEventListener('dragover', dragOver);
+            empty.addEventListener('dragenter', dragEnter);
+            empty.addEventListener('dragleave', dragLeave);
+            empty.addEventListener('drop', dragDrop);
+        }
+    
+        function dragStart() {
+            this.className += ' hold';
+            setTimeout(() => (this.className = 'invisible'), 0);
+            numberI=i;  
+            
+        }
         
-    }
-
-
-    
-    function dragEnd() {
-        this.className = 'fill';
-    }
-    
-    function dragOver(e) {
-        e.preventDefault();
-    }
-    
-    function dragEnter(e) {
-        e.preventDefault();
-        this.className += ' hovered';
-    }
-    
-    function dragLeave() {
-        this.className = 'cell';
-    }
-    
-    function dragDrop() {
-        this.className = 'cell';
-        console.log(numberI)
-        if (i==numberI) {
-            this.append(fill[i]);
+        function dragEnd() {
+            this.className = 'fill';
         }
-    }
-
-    }
-}
-
-//CHECK IF ANSWER CORRECT AND LOAD NEW GRID_________________________________________________________________
-
-function giveAnswer() {
-
-    const onClick = (event) => {
         
-        var numberFruits = document.querySelectorAll('.fill').length;
-        var answerStudent = event.target.textContent; 
-        var divisor = Math.sqrt(document.querySelectorAll('.cell').length);
-        if (answerStudent == (numberFruits % divisor)) {
-            countScore++;
-            document.getElementById("score").textContent = countScore;
-        } else {
-            countScore--;
-            document.getElementById("score").textContent = countScore;
+        function dragOver(e) {
+            e.preventDefault();
         }
-
-
-        var allDivs = document.querySelectorAll('.cell')
-        console.log(allDivs)
-        for (const allDiv of allDivs) {
-
-            allDiv.remove()
+        
+        function dragEnter(e) {
+            e.preventDefault();
+            this.className += ' hovered';
         }
-
-        generateGrid()
-        drag()
-      }
-
-
-
+        
+        function dragLeave() {
+            console.log('hein')
+            this.className = 'cell';
+        }
+        
+        function dragDrop() {
+            this.className = 'cell';
+            console.log(this.children.length)
+            if (i==numberI && this.children.length ==0) {
+                this.append(fill[i]);
+            }
+        }
     
-        var numberElms = document.querySelectorAll('.numberButton')
-        console.log(numberElms)
-        for (const numberElm of numberElms) {
-                numberElm.addEventListener('click', onClick);
         }
+    },
+    giveAnswer() { //CHECK IF ANSWER CORRECT AND LOAD NEW GRID
 
-
-
-   
-}
-
-
-
-//START GAME AS USUAL WITH THE DIFFERENT FUNCTIONS CALLED THEN_________________________________
-
-var countdown = 90;
-function startGame() {
-    state = false;
-    if (state==false) { //state manage the fact you can only once press start
-        state = true;
-        document.getElementById('start').style.color = 'transparent';
-        setInterval(timer,1000);
-        generateGrid()
-        generatePad()
-        giveAnswer()
-        drag()
-
+        const onClick = (event) => {
+            
+            var numberFruits = document.querySelectorAll('.fill').length;
+            var answerStudent = event.target.textContent; 
+            var divisor = Math.sqrt(document.querySelectorAll('.cell').length);
+            if (answerStudent == (numberFruits % divisor)) {
+                app.countScore++;
+                document.getElementById("score").textContent = app.countScore;
+            } else {
+                app.countScore--;
+                document.getElementById("score").textContent = app.countScore;
+            }
+    
+    
+            var allDivs = document.querySelectorAll('.cell')
+            console.log(allDivs)
+            for (const allDiv of allDivs) {
+    
+                allDiv.remove()
+            }
+    
+            app.generateGrid()
+            app.drag()
+          }
+    
+    
+    
+        
+            var numberElms = document.querySelectorAll('.numberButton')
+            console.log(numberElms)
+            for (const numberElm of numberElms) {
+                    numberElm.addEventListener('click', onClick);
+            }
+    
+    
+    
+       
+    },
+    timer() {
+        document.getElementById('countdown').innerHTML = app.countdown;
+    
+        if (app.countdown==0) {
+            clearInterval(app.timer);
+            alert("Gamer Over. Score: " + app.countScore);
+            location.reload();
+    
+        }
+        app.countdown--;
     }
 
+
+
 }
 
-//MANAGE TIME_________________________________________________________________________________________________________________________
+app.startGame()
 
-function timer() {
-    document.getElementById('countdown').innerHTML = countdown;
 
-    if (countdown==0) {
-        clearInterval(timer);
-        alert("Gamer Over. Score: " + countScore);
-        location.reload();
 
-    }
-    countdown--;
-}
+
