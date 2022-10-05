4ed10ccaeb8aea5020161b40b2d858f7ffe737b2
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-10-05 16:59:52 +0200

    work on scenario

--- ch1_g1_onboard/ch1_g1_onboard.js
+++ ch1_g1_onboard/ch1_g1_onboard.js
@@ -10,7 +10,7 @@ app = {
     },
     story() {
         document.getElementById('story').textContent = 'Le captaine ne se souvient plus de ses derniers jours en mer...'
-        app.text = ["Une terrible tempête s'est abbatue au large et le voici naufragé sur une île mystérieuse", "Lui et ses perroquets sont les seuls rescapés de son équipage... :'(", "Vite il faut se ressaisir! Le capitaine a atteri sur la très hostile île des nombres décimaux...","Le capitaine a besoin de ton aide","Pour que le capitaine survive, tu vas l'aider à récuperer un maximum de denrées alimentaires!","Choisis les boîtes les plus lourdes en cliquant dessus! Good luck :)"];
+        app.text = ["Une terrible tempête s'est abbatue au large et le voici naufragé sur une île mystérieuse.", "Lui et ses perroquets sont les seuls rescapés de son équipage... :'(", "Vite il faut se ressaisir! Le capitaine a atteri sur la très hostile île des nombres décimaux...","Le capitaine a besoin de ton aide.","Pour que le capitaine survive, tu vas l'aider à récuperer un maximum de denrées alimentaires!","Choisis les boîtes les plus lourdes en cliquant dessus! Good luck :)"];
         app.counter = 0;
         app.elem = document.getElementById('story');
         var inst = setInterval(app.change, 6000);
--- ch1_g2_onboard/ch1_g2_onboard.js
+++ ch1_g2_onboard/ch1_g2_onboard.js
@@ -10,7 +10,7 @@ app = {
     },
     story() {
         document.getElementById('story').textContent = `Au centre de l'île, il existe une source qui contient des nombres décimaux...`
-        app.text = ["Tout les pouvoirs mystiques de l'île proviendraient de cette source...", "Les nombres décimaux coulent le long d'une rivière que le capitaine a trouvé grâce à une vieille carte qu'il possède" ,"Il paraît même que les zéros inutiles peuvent se transformer en pièce d'or!!", "Aide le capitaine à obtenir des pièces, elles pourraient s'avérer utile pour la suite de l'aventure.","Pour cela, clique sur les zéros inutiles des différents nombres décimaux qui coulent le long de la rivière"];
+        app.text = ["Tout les pouvoirs mystiques de l'île proviendraient de cette source...", "Les nombres décimaux coulent le long d'une rivière que le capitaine a trouvé grâce à une vieille carte qu'il possède." ,"Il paraît même que les zéros inutiles peuvent se transformer en pièce d'or!!", "Aide le capitaine à obtenir des pièces, elles pourraient s'avérer utile pour la suite de l'aventure.","Pour cela, clique sur les zéros inutiles des différents nombres décimaux qui coulent le long de la rivière."];
         app.counter = 0;
         app.elem = document.getElementById('story');
         var inst = setInterval(app.change, 6000);
