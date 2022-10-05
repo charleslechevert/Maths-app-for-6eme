6476e92e9f14af769a7a52d65c479b60be13bc73
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-10-05 19:37:56 +0200

    work scenario

--- ch1_g4_onboard/ch1_g4_onboard.js
+++ ch1_g4_onboard/ch1_g4_onboard.js
@@ -10,7 +10,7 @@ app = {
     },
     story() {
         document.getElementById('story').textContent = `Le capitaine a entendu parler d'un pirate capable de réparer n'importe quel bateau`
-        app.text = ["Cependant, ce pirate est très avare et demande une montagne de pièces d'or en échange.", "Le capitaine en a récolté énormément dans la rivière mais il lui en manque encore beaucoup." ,"Les perroquets maintenant sages, le capitaine a eu une idée géniale.", "Il s'est rappelé qu'il avait un sac de chocolat dans la cale de son bateau","Le chocolat est un denrée presque aussi rare que l'or sur l'île et il va donc vendre cette ressource sur le marché!","Aide le capitaine a vendre son chocolat sur le marché!"];
+        app.text = ["Cependant, ce pirate est très avare et demande une montagne de pièces d'or en échange.", "Le capitaine en a récolté énormément dans la rivière mais il lui en manque encore beaucoup." ,"Les perroquets maintenant sages, le capitaine a eu une idée géniale.", "Il s'est rappelé qu'il avait un sac de chocolat dans la cale de son bateau","Le chocolat est une denrée presque aussi rare que l'or sur l'île et il va donc vendre cette ressource sur le marché!","Aide le capitaine a vendre son chocolat sur le marché!"];
         app.counter = 0;
         app.elem = document.getElementById('story');
         var inst = setInterval(app.change, 6000);
