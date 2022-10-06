5d4924ec3994e0d36cd96cd796c67ef9755e6878
Charles Le Chevert <charles.lechevert@outlook.fr>
2022-10-06 12:09:22 +0200

    check

--- ch3_g1_onboard/ch3_g1_onboard.js
+++ ch3_g1_onboard/ch3_g1_onboard.js
@@ -10,7 +10,7 @@ app = {
     },
     story() {
         document.getElementById('story').textContent = `Le capitaine est bien arrivé sur l'île qu'il recherchait`
-        app.text = ["C'est une immense jungle qui regorge de mystère!", "Les denrées ont été toutes écoulées en mer., il faut donc se nourir." ,"Au loin, le capitaine aperçoit des marchands qui vendent les fruits de la jungle en lot.", "Le nombre de fruit par lot correpond à la longueur du carré sur lequel sont disposés les fruits.","Les marchands sont d'accord de donner au capitaine les fruits en surplus qui ne forment pas un lot complet.","Aide le capitaine à trouver le nombre de fruits en surplus. Essaye de ne pas te tromper au risque que les marchands se fâchent!"];
+        app.text = ["C'est une immense jungle qui regorge de mystère!", "Les denrées ont été toutes écoulées en mer., il faut donc se nourir." ,"Au loin, le capitaine aperçoit des marchands qui vendent les fruits de la jungle en lot.", "Le nombre de fruit par lot correspond à la longueur du carré sur lequel sont disposés les fruits.","Les marchands sont d'accord de donner au capitaine les fruits en surplus qui ne forment pas un lot complet.","Aide le capitaine à trouver le nombre de fruits en surplus.","Tu peux déplacer les fruits afin de les réorganiser sur le carré!"];
         app.counter = 0;
         app.elem = document.getElementById('story');
         var inst = setInterval(app.change, 6000);
